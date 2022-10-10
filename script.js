ab61d036d3257eda79747675c897c4dfaeca2a26
Abhishek Singhal <abhisinghal112001@gmail.com>
2022-10-10 18:00:00 +0530

    heading

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ shownotes();
 impsecdisplay();
 important();
 
-
-
 // ================add element to local storage memory=================
-
 let addbtn = document.getElementById("addbtn")
 const on_addbtn = () => {
     let notes = localStorage.getItem("material")
+    let he_ar = document.getElementById("headArea");
     let txar = document.getElementById("txtArea");
     if (notes == null) {
         notesarr = [];
@@ -16,20 +14,15 @@ const on_addbtn = () => {
     else {
         notesarr = JSON.parse(notes);
     }
-    if (txar.value != "")
-        notesarr.push(txar.value);
+    if (txar.value != "" && he_ar.value!=""){
+        notesarr.push([he_ar.value, txar.value.replace(/\s\n\s/gi, "<br/>").replace(/\s\n/gi, "<br/>").replace(/\n\s/gi, "<br/>").replace(/\n/gi, "<br/>").replace(/\s{2,}/g, ' ')]);
+    }
     localStorage.setItem("material", JSON.stringify(notesarr));
     txar.value = '';
+    he_ar.value = '';
     shownotes();
 }
 addbtn.addEventListener("click", on_addbtn)
-
-
-document.addEventListener("keypress",(e)=>{
-    if(e.key==="Enter"){
-        on_addbtn();
-    }
-})
 
 // ===========display the cards on DOM============== 
 function shownotes() {
@@ -50,17 +43,17 @@ function shownotes() {
         stararr = JSON.parse(star);
     }
     notesarr.forEach((e, index) => {
-        flag=0
+        flag = 0
         stararr.forEach((se) => {
-             if(se==index)
-                flag++; 
-            });
-        if (flag != 0 ) {
+            if (se == index)
+                flag++;
+        });
+        if (flag != 0) {
             writedown.innerHTML += `
-                    <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="shadow noteCard my-2 mx-2 card" style="width: 18rem;">
                         <div class="card-body">
-                            <h5 class="card-title cb">Note ${index + 1}</h5>
-                            <p class="card-text cb1">${e}</p>
+                            <h5 class="card-title cb">${e[0]}</h5>
+                            <p class="card-text cb1">${e[1]}</p>
                             <button id="${index}"onclick="deletecard(this.id)" class="btn btn-danger">Delete Note</button>
                             <span class="impMsg">Important!</span>
                             <i class="bi bi-star-fill imp1"></i>
@@ -70,10 +63,10 @@ function shownotes() {
         }
         else {
             writedown.innerHTML += `
-                    <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="shadow noteCard my-2 mx-2 card" style="width: 18rem;">
                         <div class="card-body">
-                            <h5 class="card-title cb">Note ${index + 1}</h5>
-                            <p class="card-text cb1">${e}</p>
+                            <h5 class="card-title cb"> ${e[0]}</h5>
+                            <p class="card-text cb1">${e[1]}</p>
                             <button id="${index}"onclick="deletecard(this.id)" class="btn btn-danger">Delete Note</button>
                             <span class="impMsg">Important!</span>
                             <i class="bi bi-star imp1"></i>
@@ -86,39 +79,40 @@ function shownotes() {
 
 }
 
-
-
 // =================for important section============
-function important()
-{let data = document.querySelectorAll(".noteCard");
-data.forEach((e,index) => {
-    let halfStar = e.getElementsByTagName("i")[0];
-    let impMsg = e.getElementsByTagName("span")[0];
-    halfStar.addEventListener("mouseenter", () => {
-        impMsg.style.display = "block";
-    })
-    halfStar.addEventListener("mouseout", () => {
-        impMsg.style.display = "none";
-    })
-    halfStar.addEventListener("click", () => {
-        if (halfStar.classList[1] == "bi-star") {
-            halfStar.className = 'bi bi-star-fill imp1';
-            notesarr.forEach((ne,index)=>{
-                if(e.getElementsByTagName('p')[0].innerText==ne)
-                impsecadd(index);
-            })
-        }
-        else if (halfStar.classList[1] == "bi-star-fill") {
-            halfStar.className = 'bi bi-star imp1';
-            notesarr.forEach((ne,index)=>{
-                if(e.getElementsByTagName('p')[0].innerText==ne)
-                impsecremove(index);
-            })
+function important() {
+    let data = document.querySelectorAll(".noteCard");
+    data.forEach((e, index) => {
+        let halfStar = e.getElementsByTagName("i")[0];
+        let impMsg = e.getElementsByTagName("span")[0];
+        halfStar.addEventListener("mouseenter", () => {
+            impMsg.style.display = "block";
+        })
+        halfStar.addEventListener("mouseout", () => {
+            impMsg.style.display = "none";
+        })
+        halfStar.addEventListener("click", () => {
+            if (halfStar.classList[1] == "bi-star") {
+                halfStar.className = 'bi bi-star-fill imp1';
+                notesarr.forEach((ne, index) => {
+                    console.log(e.getElementsByTagName('p')[0].innerText.replace(/\n/gi, "<br/>"))
+                    console.log(ne[1]);
+                    if (e.getElementsByTagName('p')[0].innerText.replace(/\n/gi, "<br/>") == ne[1]) { 
+                        impsecadd(index); 
+                    }
+                })
+            }
+            else if (halfStar.classList[1] == "bi-star-fill") {
+                halfStar.className = 'bi bi-star imp1';
+                notesarr.forEach((ne, index) => {
+                    if (e.getElementsByTagName('p')[0].innerText == ne[1])
+                        impsecremove(index);
+                })
 
-        }
-    })
-});
-// shownotes();
+            }
+        })
+    });
+    // shownotes();
 }
 
 // =========to import stared card index no in the local storage of 'star'=====
@@ -151,8 +145,8 @@ function impsecdisplay() {
         impsec.innerHTML += `
         <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
         <div class="card-body">
-        <h5 class="card-title cb">Note ${e + 1}</h5>
-        <p class="card-text cb1">${notesarr[e]}</p>
+        <h5 class="card-title cb">${notesarr[e][0]}</h5>
+        <p class="card-text cb1">${notesarr[e][1]}</p>
         <button id="${e}"onclick="deletecard(this.id)" class="btn btn-danger">Delete Note</button><i>
         <span class="impMsg">Important!</span>
         </div>
@@ -174,12 +168,12 @@ function impsecremove(notodel) {
 
 // ===============this will delete the card =============
 function deletecard(notodel) {
-    notesarr.splice(notodel,1);
+    notesarr.splice(notodel, 1);
     localStorage.setItem('material', JSON.stringify(notesarr));
     stararr.forEach((e, index) => {
         if (e == notodel)
             stararr.splice(index, 1);
-        else if(e>notodel)
+        else if (e > notodel)
             stararr[index]--;
         localStorage.setItem('star', JSON.stringify(stararr));
     })
@@ -194,7 +188,8 @@ search.addEventListener('input', () => {
     let data = document.querySelectorAll(".noteCard");
     data.forEach((e) => {
         let element = e.getElementsByTagName('p')[0].innerText.toLowerCase();
-        if (element.includes(search.value.toLowerCase())) {
+        let element1 = e.getElementsByTagName('h5')[0].innerText.toLowerCase();
+        if (element.includes(search.value.toLowerCase()) || element1.includes(search.value.toLowerCase())) {
             e.style.display = "block"
         }
         else {
